3b2f9c64a644de1c6c65859fd6f5e4b1d3ef51e2
Jakub Worek <worekkuba@gmail.com>
2023-12-12 13:34:28 +0100

    orders table update

--- pages/orders.js
+++ pages/orders.js
@@ -18,6 +18,7 @@ export default function OrdersPage(){
         <thead>
           <tr>
             <th>Date</th>
+            <th>Paid</th>
             <th>Customer</th>
             <th>Products</th>
           </tr>
